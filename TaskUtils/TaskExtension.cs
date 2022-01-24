0b788162d4259b7928d5c71a7541b349f2ef9364
Tom <zivotsky@ginasystem.com>
2022-01-24 23:34:33 +0100

    task extension - when all

---
+++ TaskUtils/TaskExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskUtils;
+
+public static class TaskExtension
+{
+    public static async Task<IEnumerable<T>> WhenAll<T>(params Task<T>[] tasks)
+    {
+        var allTasks = Task.WhenAll(tasks);
+
+        try
+        {
+            return await allTasks;
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+
+        throw allTasks.Exception ?? throw new Exception("Aggregated exception is not set.");
+    }
+}
